0b973e10b7217af8efc6f11b627023db6ef6dbf3
Yash Raundal <yraundal03@gmail.com>
2026-07-21 13:01:28 +0530

    Time: 6 ms (28.95%), Space: 42.6 MB (63.19%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int count = 0;
+        for(int i=1;i<=n;i++){
+            n = n-i;
+            count++;
+        }
+        return count;
+    }
+}
